492a017db14a1b966c1089490d81e5ed87e8161b
Mihretthe <mihretekalgn@gmail.com>
2026-01-18 23:23:48 +0300

    deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn diabetic_prediction.wsgi
---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
