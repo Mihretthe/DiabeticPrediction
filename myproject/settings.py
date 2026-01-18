3c1bda97a2dac83a8b6ec0a02f25ea6c376bcb0d
Mihretthe <mihretekalgn@gmail.com>
2026-01-18 23:34:59 +0300

    adding static to setting

--- myproject/settings.py
+++ myproject/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'diabetic'
 ]
 
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -118,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
